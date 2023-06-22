a8987b39b02b42e40b5b67fc0ea452c9958dc981
rahulgi8796 <112067490+rahulgi8796@users.noreply.github.com>
2023-06-22 13:23:50 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request,'base.html')
+
+
+
+
+def index1(request):
+    return render(request,'base.html')
+def index1(request):
+    return render(request,'base.html')
+def index1(request):
+    return render(request,'base.html')
+def index1(request):
+    return render(request,'base.html')
+
+
+
+
+
+
